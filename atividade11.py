8c432fb4e5ec2fbaba31487a5755ee853a736917
sofia <sofiaelena08200@gmail.com>
2024-10-27 00:02:33 -0300

    exercicio 11

--- atividade11.py
+++ atividade11.py
@@ -1,1 +1,17 @@
-#aaaaaaaaaaaaaaaaaaaa
+#Faça um programa que simule o lançamento de um dado. O usuário deve escolher quantos dados jogar e o programa deve exibir os resultados.
+
+import random
+
+quantos_dados = int(input("Quantos dados vai jogar?: "))
+quantos_lados = int(input("Quantos lados tem seus dados?: "))
+
+resultados = []
+
+for _ in range(quantos_dados):
+    resultado = random.randint(1, quantos_lados)
+    resultados.append(resultado)
+
+print("Os resultados são: ")
+for i, resultado in enumerate(resultados, start=1):
+    print(f"Dado {i}: {resultado}")
+
